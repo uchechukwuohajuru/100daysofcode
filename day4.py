a11374a6639880f52a59333cc3d3f81023129e7c
uchechukwuohajuru <uchechukwuohajuru@gmail.com>
2025-09-05 01:34:44 +0100

    This is my attempt to add some other files and ignore others

---
+++ day4.py
@@ -0,0 +1,72 @@
+# Python Operators and Expressions
+
+'''
+We are on day 4 already! Today, we talk about Operators and expressions in python.
+Operators are generally our way of helping python make decisions and take actions. And we have a number of them that we are going to
+talk about.
+'''
+# Operators are symbols or keywords that we use to perform operation on one or more values or variables. 
+# The variables in an operation are known as operands.
+
+'''
+We will categories them into:
+1. Arithematic operators
+2. Logical Operators
+3. Comparison Operators
+4. Assignment Operators
+5. Identity Operators
+6. Membership Operators
+'''
+
+# Arithematic Operators 
+'''
+(+) Addition Operator: This is used when we want ot sum two or more variables.
+(-) Subtraction Operator: This is used when we want to subtract one variable from another.
+(*) Multiplication Operator: This is used to get the product of two or more variables.
+(**) Exponential Operator: This is used when we want to get the exponent of two or more variables.
+(/) Division Operator: This is used to get the dividend of two or more variables.
+(//) Floor division Operator: This is used when we want to get only the whole number part of a dividend
+(%) Modulo Division Operator: This is used when we want to get only the fraction (decimal) part of a dividend.
+'''
+
+# Assignment Operators
+'''
+These are operators we use in assigning values to variables. Example:
+(=) Assignment Operator: This is used to assign values to variables
+(+=) Increment Assignment Operator: This increases a value and assigns it to the variable
+(-=) Decrement Assignment Operator: This decreases a value and then assigns it to the variable
+*** Any arithematic operator can be combined with an assignment operator to satisfy a uniqe use case.
+'''
+# Comparison Operators
+'''
+These are also called relational operators because they are used to compare one variable or value with another.
+They return Boolean results. Example:
+(==) Equal To Operator: This checks if two values or variables are equal
+(!=) Not Equal To Operator: This checks if two variables are not equal
+(>) Greater Than Operator: This checks if the variable on the right is bigger than the variable on the left
+(<) Less Than Operator: This checks if the variable on the right is less than the variable on the left
+(>=) Greater Than or Equal To Operator: This checks if the variable on the right is greater  than or equal to the variable on the left
+(<=) Less Than or Equal To Operator: This checks if the variable on the right is less than or equal to the variable on the right
+'''
+# Logical Operators
+'''
+These are also called Boolean operators because they are used to combine conditional statements and evaluate Boolean expressions.
+(and) Logical AND: This returns a True result if the two combined conditional statements are True or False. It returns False if 
+any of them is False.
+(or) Logical OR: This returns a True result if any one of the combined conditional statements is True
+(not) Logical NOT: This returns a True result when non of the combined conditional statements is True
+'''
+
+# Identity Operators
+'''
+These are operators used to check if two variables refere to the same object in memory. They return Boolean results.
+(is): shows that the object is the same
+(is not): shows that the object is not the same
+'''
+
+# Membership Operators
+'''
+These are used to test for membership in a sequence.
+(in): shows that the item is a member of the sequence
+(not in): shows that the item is not a member of the squence
+'''
---
+++ day5.py
@@ -0,0 +1,27 @@
+# Expresions in python
+
+'''
+This is where the challenge gets more interesting because expressions form the foundation of all python codes.
+
+In python, expresions are combinations of values, variables, operators and functions in a way that python interprets 
+it to produce a single result. Example:
+'''
+
+print('==================\n')
+x = 6 # this assigns the value 6 to a variable x
+y = 15 # this assigns the value 15 to the variable y
+
+z =x+y # this is now is an expression in python. And when I execute it, a result (21) which can be seen using
+#the print() function is displayed.
+print(z)
+
+print('==================\n')
+
+# Let's have some more examples
+
+age = 15
+print(age>18) #Remembr > is a comparison operator and evaluates to True or False. The result if False because age is not greater than 18
+
+print('====================\n')
+
+
